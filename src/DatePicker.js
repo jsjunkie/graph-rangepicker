1a5b14f6da49919c13018cb1ce5b31046651dad1
jsjunkie <c.gulab@gmail.com>
2018-05-24 23:57:05 +0530

    make UI better

--- src/DatePicker.js
+++ src/DatePicker.js
@@ -6,20 +6,20 @@ export default class DatePicker extends Component {
     super();
 
     this.state = {
-      bars: [20, 30, 10, 33, 22]
+      bars: [30, 60, 10, 83, 12]
 
     }
   }
 
   render() {
-    const areaLength = this.state.bars.length * 20;
+    const areaLength = this.state.bars.length * 22;
     return (
       <div>
         <div className="graph">
-          {this.state.bars.map((bar, i) => <div key={i} style={{height: bar, left:20*i, bottom: 0}} className="bar bar-light"></div>)}
+          {this.state.bars.map((bar, i) => <div key={i} style={{height: bar, left:22*i, bottom: 0}} className="bar bar-light"></div>)}
           <div className="box" style={{ marginLeft: areaLength, width: areaLength }}></div>
-          {this.state.bars.map((bar, i) => <div key={i} style={{ height: bar, left: areaLength + 20 * i, bottom: 0 }} className="bar bar-dark"></div>)}
-          {this.state.bars.map((bar, i) => <div key={i} style={{ height: bar, left: areaLength * 2 + 20 * i, bottom: 0 }} className="bar bar-light"></div>)}
+          {this.state.bars.map((bar, i) => <div key={i} style={{ height: bar, left: areaLength + 22 * i, bottom: 0 }} className="bar bar-dark"></div>)}
+          {this.state.bars.map((bar, i) => <div key={i} style={{ height: bar, left: areaLength * 2 + 22 * i, bottom: 0 }} className="bar bar-light"></div>)}
         </div>
         <div className="scroll">
           <div className="slider" style={{ left: areaLength - 20, top: -5 }}>||</div>
